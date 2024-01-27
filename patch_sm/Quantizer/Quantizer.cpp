bd4b836315b30c3192096abb7d29bbd47978da8a
April Lee <april.marina@proton.me>
2024-01-27 16:54:06 -0600

    Quantize to scale
    For now the quantizer always goes
    up to the next highest note in the
    scale instead of finding the nearest.
    The scale still needs to be made
    user-assignable.

--- patch_sm/Quantizer/Quantizer.cpp
+++ patch_sm/Quantizer/Quantizer.cpp
@@ -18,24 +18,17 @@ int main(void)
 {
     // This is the set of all notes in the 12-tone scale. True notes are active
     // and can be quantized to, while false notes are disabled.
-    std::array<bool, 12> notes = { true, true, true, true, true, true, true, true, true, true, true, true };
     bool n[12] = { true, false, true, true, false, false, false, true, true, false, false, false };
 
     /** Initialize the patch_sm hardware object */
     patch.Init();
 
-    float led_brightness = 0.f;
-
     /** Loop forever */
     while(1)
     {
-        //patch.ProcessAllControls();
         patch.ProcessAllControls();
         bool state = patch.gate_in_1.State();
-        //patch.WriteCvOut(CV_OUT_2, state ? 5.0 : 0.0);
         patch.SetLed(state);
-        // Only update CV output when a gate signal is received.
-        //if (!state) continue;
 
         // What is the allowed range of output for this value? Is it different
         // for different CV inputs?
@@ -47,11 +40,26 @@ int main(void)
         // Keystep 37 (outputs 0v - 10v). This mapping perfectly aligns the
         // Keystep output with out CV output.
         float voct = fmap(voct_cv, 0.f, 5.f);
-        patch.WriteCvOut(CV_OUT_2, voct);
+        //patch.WriteCvOut(CV_OUT_2, voct);
 
         // Now all notes are quantized to the 12-tone scale.
-        const float semi = voct / semitone;
-        const float out = roundf(semi) * semitone;
+        const float semi = floorf(voct / semitone);
+        int nx = static_cast<int>(semi) % 12;
+        // Show if input voltage was in the current scale (after round)
+        if (n[nx]) patch.WriteCvOut(CV_OUT_2, 5);
+        else patch.WriteCvOut(CV_OUT_2, 0);
+        // Only update CV output when a gate signal is received.
+        if (!state) continue;
+        float add = 0.f;
+        // For now always go up to the next note. Later we'll find the nearest
+        // note instead.
+        while (!n[nx % 12])
+        {
+            ++nx;
+            add += 1.f;
+        }
+
+        const float out = (semi + add) * semitone;
         patch.WriteCvOut(CV_OUT_1, out);
     }
 }
--- patch_sm/Quantizer/README.md
+++ patch_sm/Quantizer/README.md
@@ -8,6 +8,8 @@ April Lee
 
 This is a quantizer that allows the user to specify a scale that incoming CV 
 intput on `CV_5` will be quantized to. Quantized CV is output to `CV_OUT_1`.
+The output CV will only be updated when a gate trigger is present on 
+`gate_in_1`.
 
 ## Build
 
