d0620ad2dc9fdbd315697457a27e61b8160afa5e
April Lee <april.marina@proton.me>
2024-01-27 13:13:51 -0600

    Add v/oct mapping and 12-tone quantize
    I don't understand the way CV input works exactly
    but fmapping the input to 0v - 5v perfectly
    aligns the CV output of my Keystep 37 with the
    CV output. i.e. passthrough will relay the
    correct note. Notes are divided by 1/12 and
    rounded to get a quantization to the 12-tone
    scale. Further work is needed to eliminate
    notes and create other scales that input
    can be quantized to.

--- patch_sm/Quantizer/Quantizer.cpp
+++ patch_sm/Quantizer/Quantizer.cpp
@@ -18,7 +18,8 @@ int main(void)
 {
     // This is the set of all notes in the 12-tone scale. True notes are active
     // and can be quantized to, while false notes are disabled.
-    std::array<bool, 12> notes = { 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0 };
+    std::array<bool, 12> notes = { true, true, true, true, true, true, true, true, true, true, true, true };
+    bool n[12] = { true, false, true, true, false, false, false, true, true, false, false, false };
 
     /** Initialize the patch_sm hardware object */
     patch.Init();
@@ -28,64 +29,29 @@ int main(void)
     /** Loop forever */
     while(1)
     {
+        //patch.ProcessAllControls();
         patch.ProcessAllControls();
-        // const bool state = patch.gate_in_1.State();
-        // // Only update CV output when a gate signal is received.
-
-        // if (!state) {
-        //     //if (led_brightness > 0.f) led_brightness -= 0.5f;
-        //     patch.WriteCvOut(CV_OUT_2, 0);
-        // }
-        // else
-        // {
-        //     patch.WriteCvOut(CV_OUT_2, 5);
-
-        // }
-        //led_brightness = 5.f;
+        bool state = patch.gate_in_1.State();
+        //patch.WriteCvOut(CV_OUT_2, state ? 5.0 : 0.0);
+        patch.SetLed(state);
+        // Only update CV output when a gate signal is received.
+        //if (!state) continue;
 
         // What is the allowed range of output for this value? Is it different
         // for different CV inputs?
-        const float value = patch.GetAdcValue(CV_5);
-        const float octave = floorf(value);
-        const float note = (value - octave) / semitone;
-        bool found = false;
-        int distance = 1;
-        const int index = round(note);
-        // If the note is on the high end, we start by looking up the scale,
-        // else we look down first.
-        const int multiplier = (note / 1.f) > 0.5 ? 1 : -1;
-        float output = 0.f;
-        // If note is 3.55, it's closer to 4 than 3, but closer to 3 than 5.
-        // Try four, then 3, then 5, then 2, etc. Be sure to circle around, not
-        // overflow the buffer.
-        if (notes.at(index)) {
-            output = octave + index * semitone;
-            found = true;
-        }
-        // If no notes are currently active the CV output will never update...
-        while (!found)
-        {
-            int d = index + distance * multiplier;
-            // Is std::array::at a better choice here?
-            // Our search for the next active note wraps around the scale, but
-            // it should go up an octave then ...
-            int access = d % notes.size();
-            if (notes[access])
-            {
-                output = octave + access * semitone;
-                found = true;
-                break;
-            }
-            int negative_access = -d % notes.size();
-            if (notes[negative_access])
-            {
-                output = octave + negative_access * semitone;
-                found = true;
-                break;
-            }
-            distance++;
-        }
 
-        patch.WriteCvOut(CV_OUT_1, output);
+        // The voltage range for the CV inputs are +-pIn.
+        // But I'm pretty sure the output of this call is +-1v. See
+        // InitBipolarCV definition.
+        const float voct_cv = patch.GetAdcValue(CV_5);
+        // Keystep 37 (outputs 0v - 10v). This mapping perfectly aligns the
+        // Keystep output with out CV output.
+        float voct = fmap(voct_cv, 0.f, 5.f);
+        patch.WriteCvOut(CV_OUT_2, voct);
+
+        // Now all notes are quantized to the 12-tone scale.
+        const float semi = voct / semitone;
+        const float out = roundf(semi) * semitone;
+        patch.WriteCvOut(CV_OUT_1, out);
     }
 }
--- patch_sm/Quantizer/README.md
+++ patch_sm/Quantizer/README.md
@@ -1,4 +1,4 @@
-# Quamtizer
+# Quantizer
 
 ## Author
 
@@ -6,4 +6,12 @@ April Lee
 
 ## Description
 
-<!-- Describe your example here -->
+This is a quantizer that allows the user to specify a scale that incoming CV 
+intput on `CV_5` will be quantized to. Quantized CV is output to `CV_OUT_1`.
+
+## Build
+
+Here's the command to build the program on the target device, after resetting 
+it by holding `boot` and pressing `reset`.
+
+`make clean; make; make program-dfu`
