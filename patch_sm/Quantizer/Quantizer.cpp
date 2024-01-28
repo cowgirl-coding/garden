6abd1da57d6525a9f3ad55190104e59844b361d1
April Lee <april.marina@proton.me>
2024-01-28 17:26:04 -0600

    Update quantizer and include scales
    Scales for the quanitizer are represented as
    16-bit unsigned ints whose 12 least significant
    bits each represent a note in the even-tempered
    12-tone scale. The binary pattern specifies which
    notes are active. The quantizer will search for
    the nearest possible note in the scale to the
    input CV value.

--- patch_sm/Quantizer/Quantizer.cpp
+++ patch_sm/Quantizer/Quantizer.cpp
@@ -14,11 +14,53 @@ DaisyPatchSM patch;
 conveniently linear. */
 constexpr float semitone = 1.f / 12.f;
 
+// ** Built-in Scales **
+// Here we represent the scale as a 16-bit int, where the notes in the
+// 12-tone scale are listed as active or inactive from right to left. The
+// least significant bit denotes the root of the scale, while the most
+// significant denotes the major 7th.
+// Western modes
+constexpr uint16_t twelve_tone =0b111111111111;
+constexpr int16_t ionian =      0b101010110101;
+constexpr int16_t dorian =      0b011010101101;
+constexpr int16_t aeolian =     0b010110101101;
+constexpr int16_t hirajoshi =   0b000110001101;
+constexpr int16_t whole_tone =  0b010101010101;
+constexpr int16_t maj_7 =       0b100010010001;
+constexpr int16_t min_7 =       0b010010001001;
+// TODO: Add insen, iwato, pelog
+
+// Find the minimum distance we can go from our note value to an index
+// of an active note. This wraps, so 11 is only 1 away from zero.
+// Round the note and take as first index. Then take both notes 1 index
+// away. If only one, take it. If both, compare distance. If neither,
+// set distance to 2 and repeat.
+int8_t find_distance(const uint16_t scale, const float note)
+{
+    const uint8_t index = round(note);
+    int8_t distance = 1;
+    for (; distance < 13; ++distance)
+    {
+        const bool positive = scale >> ((index + distance) % 12) & 1;
+        const bool negative = scale >> ((index - distance) % 12) & 1;
+        //if (positive) break;
+        //if (negative) { distance = -distance; break; }
+        if (!(positive || negative)) continue;
+        if (positive && negative) { 
+            const uint8_t d = abs(note - (index + distance));
+            const uint8_t e = abs(note - (index - distance));
+            return d < e ? distance : -distance;
+        }
+        return positive ? distance : -distance;
+    }
+    // This would happen if the scale was 0x0;
+    return 0;
+};
+
 int main(void)
 {
-    // This is the set of all notes in the 12-tone scale. True notes are active
-    // and can be quantized to, while false notes are disabled.
-    bool n[12] = { true, false, true, true, false, false, false, true, true, false, false, false };
+    // TODO: Allow user to select or define the scale
+    const uint16_t scale = whole_tone;
 
     /** Initialize the patch_sm hardware object */
     patch.Init();
@@ -26,40 +68,43 @@ int main(void)
     /** Loop forever */
     while(1)
     {
+
         patch.ProcessAllControls();
         bool state = patch.gate_in_1.State();
         patch.SetLed(state);
-
-        // What is the allowed range of output for this value? Is it different
-        // for different CV inputs?
-
-        // The voltage range for the CV inputs are +-pIn.
-        // But I'm pretty sure the output of this call is +-1v. See
-        // InitBipolarCV definition.
-        const float voct_cv = patch.GetAdcValue(CV_5);
-        // Keystep 37 (outputs 0v - 10v). This mapping perfectly aligns the
-        // Keystep output with out CV output.
-        float voct = fmap(voct_cv, 0.f, 5.f);
-        //patch.WriteCvOut(CV_OUT_2, voct);
-
-        // Now all notes are quantized to the 12-tone scale.
-        const float semi = floorf(voct / semitone);
-        int nx = static_cast<int>(semi) % 12;
-        // Show if input voltage was in the current scale (after round)
-        if (n[nx]) patch.WriteCvOut(CV_OUT_2, 5);
-        else patch.WriteCvOut(CV_OUT_2, 0);
         // Only update CV output when a gate signal is received.
         if (!state) continue;
-        float add = 0.f;
-        // For now always go up to the next note. Later we'll find the nearest
-        // note instead.
-        while (!n[nx % 12])
-        {
-            ++nx;
-            add += 1.f;
-        }
+        // Only quntize if there's a value to quantize to
+        if (scale == 0) continue; 
 
-        const float out = (semi + add) * semitone;
+        // The voltage range for this CV input is -5v to 5v. However the return
+        // value from GetAdcValue is normalized to -1v to 1v. It's scaled by
+        // AnalogControl. I asked about this here:
+        // https://discord.com/channels/1037767234803740694/1039589722831474830/1201020358078234635
+        const float voct_cv = patch.GetAdcValue(CV_5);
+        // This mapping restores the CV input value to a five octave range. The
+        // range of the CV output is 0-5v
+        float voct = fmap(voct_cv, 0.f, 5.f);
+
+        /* TODO: Add offset knob to help with input CV calibration */
+
+        const float octave = floorf(voct / 1.f);
+        const float note = (voct - octave) / semitone;
+        
+        // If we can quantize the current input to the nearest semitone, we're
+        // all done!
+        const uint8_t nearest_index = round(note);
+        if (scale >> (nearest_index % 12) & 1)
+        {
+            patch.WriteCvOut(CV_OUT_1, octave + roundf(note) * semitone);
+            continue;
+        }
+        // Use find disance to find the nearest active note in the scale. This
+        // can be positive or negative. For example, if the note value is 3.25,
+        // the nearest active semitone could be 5 (distance will be 2), or it
+        // could be 2 if 2 is active (distance will be -1).
+        const int8_t distance = find_distance(scale, note);
+        const float out = octave + (roundf(note) + distance) * semitone;
         patch.WriteCvOut(CV_OUT_1, out);
     }
 }
